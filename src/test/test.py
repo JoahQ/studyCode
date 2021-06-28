e51d5a3f5c3e34450a37e4b09f7be80b9ffda4cc
blabla <912757884@qq.com>
2021-06-28 16:40:51 +0800

    abc

--- src/test/test.py
+++ src/test/test.py
@@ -1,7 +1,14 @@
+# -*-coding: utf-8 -*-
 import arcpy
 
 def a():
-    print 1
+    with arcpy.da.SearchCursor(fc, "ww") as cursor:
+        for row in cursor:
+            if "None".strip() == row[0].strip():
+                print str(row[0]) + "ii"
+            if "020" in row[0]:
+                print row[0]
 
 if __name__ == "__main__":
+    fc =  r'D:\gisdata\a.gdb\复制'
     a()
