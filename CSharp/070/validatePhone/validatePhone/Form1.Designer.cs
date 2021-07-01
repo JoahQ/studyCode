0315b5623d89f3a594c0f1794882053cd6e5c426
blabla <912757884@qq.com>
2021-07-01 15:20:02 +0800

    070

---
+++ CSharp/070/validatePhone/validatePhone/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace validatePhone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.VPButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "电话号码：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "号码格式：    XXX-XXXXXXXX";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "电话号码验证";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.VPButton);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(79, 273);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(508, 170);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "密码必须由数字和26个英文字母组成的密码字符";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "密码：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(114, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(304, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // VPButton
+            // 
+            this.VPButton.Location = new System.Drawing.Point(114, 119);
+            this.VPButton.Name = "VPButton";
+            this.VPButton.Size = new System.Drawing.Size(304, 36);
+            this.VPButton.TabIndex = 2;
+            this.VPButton.Text = "验证密码";
+            this.VPButton.UseVisualStyleBackColor = true;
+            this.VPButton.Click += new System.EventHandler(this.VPButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 488);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "用正则表达是验证电话号码";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button VPButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CSharp/070/validatePhone/validatePhone/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Forms;
+
+namespace validatePhone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 验证电话号码格式是否正确
+        /// </summary>
+        /// <param name="str_telephone">电话号码信息</param>
+        /// <returns>方法返回布尔值</returns>
+        public bool IsTelephone(string str_telephone)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(str_telephone, @"^(\d{3,4}-)?\d{6,8}$");
+        }
+
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            if (!IsTelephone(textBox1.Text))
+            { MessageBox.Show("电话号码格式不正确！"); }
+            else
+            { MessageBox.Show("电话号码格式正确！"); }
+        }
+
+
+        public bool IsPassword(string str_password)
+        {
+            return
+              System.Text.RegularExpressions.Regex.IsMatch
+                (str_password, @"[A-Za-z]+[0-9]");
+        }
+
+        private void VPButton_Click(object sender, System.EventArgs e)
+        {
+            if (!IsPassword(textBox2.Text.Trim()))
+            {
+                MessageBox.Show("密码格式不正确！");
+            }
+            else
+            {
+                MessageBox.Show("密码格式正确！");
+            }
+        }
+    }
+}
