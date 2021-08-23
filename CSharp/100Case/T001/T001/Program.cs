c14cbfa2b365b61e8bb545c48969f6aa81d289fe
qwvbv <912757884@qq.com>
2021-08-23 15:58:54 +0800

    abc

---
+++ CSharp/100Case/T001/T001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello world";
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
