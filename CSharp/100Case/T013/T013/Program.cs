382552b6984c75c56c697fd11769c9346ee097c0
qwvbv <912757884@qq.com>
2021-08-24 17:48:41 +0800

    abc

---
+++ CSharp/100Case/T013/T013/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("请输入一个整数：");
+                int Number = int.Parse(Console.ReadLine());
+                Console.WriteLine("{0}的十六进制形式为0x{1:X8}", Number, Number);
+                int NumberHex = Number & 0x000000FF;//将数值与0xFF按位与运算
+                Console.WriteLine("第1个字节为：0x{0:X2}", NumberHex);
+
+                NumberHex = Number >> 8 & 0x000000FF;//将数值右移8位与0xFF按位与运算
+                Console.WriteLine("第2个字节为：0x{0:X2}", NumberHex);
+
+                NumberHex = Number >> 16 & 0x000000FF;//将数值右移16位与0xFF按位与运算
+                Console.WriteLine("第3个字节为：0x{0:X2}", NumberHex);
+
+                NumberHex = Number >> 24 & 0x000000FF;//将数值右移24位与0xFF按位与运算
+                Console.WriteLine("第4个字节为：0x{0:X2}", NumberHex);
+
+            } while (Console.ReadLine() != "q");
+        }
+    }
+}
---
+++ CSharp/100Case/T014/T014/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("请输入学生成绩：");
+                int Result = int.Parse(Console.ReadLine());
+
+                if (Result >= 85)
+                    Console.WriteLine("学生成绩为 A");
+                if (Result < 85 && Result >= 70)
+                    Console.WriteLine("学生成绩为 B");
+                if (Result < 70 && Result >= 60)
+                    Console.WriteLine("学生成绩为 C");
+                if (Result < 60)
+                    Console.WriteLine("学生成绩为 D");
+
+
+            } while (Console.ReadLine() != "q");
+        }
+    }
+}
