d5e0e77daf6a9cbcfe83ca77bdfa3732a107dc79
qwvbv <912757884@qq.com>
2021-08-24 16:52:03 +0800

    abc

---
+++ CSharp/100Case/T009/T009/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T009
+{
+    class Program
+    {
+        class Circle
+        {
+            const double PI = 3.1415926f;
+            public readonly int Radius = 1;
+
+            public Circle(int radius)
+            {
+                Radius = radius;
+            }//构造函数
+
+            public double Area()
+            {
+                return PI * Radius * Radius;
+            }//计算圆形面积
+        }
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(10);
+            //创建一个半径为10的圆形实例
+            Console.WriteLine("半径为{0}的圆形面积为{1}", circle.Radius,circle.Area());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/100Case/T010/T010/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T010
+{
+    class Program
+    {
+        class Rectangle
+        {
+            public int Width, Height;//声明整型变量表示矩形的宽和高
+            public int TryArea()//计算矩形面积并检测数据溢出
+            {
+                checked
+                {
+                    int area = Width * Height;
+
+                    return area;
+                }
+            }
+
+            public int Area()
+            {
+                return Width * Height;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            Console.WriteLine("请输入矩形宽度：");
+            rect.Width = int.Parse(Console.ReadLine());
+            //从控制台输入数据给矩形的宽赋值
+
+            Console.WriteLine("请输入矩形高度：");
+            rect.Height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("矩形面积为：\n{0}", rect.Area());
+
+            Console.WriteLine("矩形面积为：\n{0}", rect.TryArea());
+
+            Console.ReadLine();
+        }
+    }
+}
