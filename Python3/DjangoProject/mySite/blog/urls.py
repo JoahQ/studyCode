3fe5ab45bf34863cccd923d05e45a3a97566a7a3
zhouqin <1185917912@qq.com>
2021-11-24 08:26:03 +0800

    20211124

--- Python3/DjangoProject/mySite/blog/urls.py
+++ Python3/DjangoProject/mySite/blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.blog_title),
+    path('<int:article_id>/', views.blog_article),
 ]
--- Python3/DjangoProject/mySite/blog/views.py
+++ Python3/DjangoProject/mySite/blog/views.py
@@ -7,3 +7,9 @@ from .models import BlogArticles
 def blog_title(request):
     blogs = BlogArticles.objects.all()
     return render(request, "blog/titles.html", {"blogs": blogs})
+
+
+def blog_article(requst, article_id):
+    article = BlogArticles.objects.get(id=article_id)
+    pub = article.publish
+    return render(requst, "blog/content.html", {"article": article, "publish": pub})
