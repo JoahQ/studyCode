5a7bef5e9f8d692fd30211b4aefbed486850732b
QinZhou <1185917912@qq.com>
2022-05-02 23:30:17 +0800

    add golang

---
+++ golang/book/e14/14.3.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const Foo string = "constant string"
+
+func main() {
+	fmt.Printf("Foo: %v\n", Foo)
+	a_string := "hello"
+	fmt.Printf("a_string: %v\n", a_string)
+}
